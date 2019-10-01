b63ae581a322cd52a58916d00f5789e923b99cce
Fabiano Matheus B Dutra <fmbdutra@gmail.com>
2019-10-01 16:41:00 -0300

    configurado banco mongo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
+// Banco feito em Mongo Atlas. Base de teste.
+// Escolhido compatível com versao abaixo de 3.
 const app = express();
+mongoose.connect('mongodb://fabiano:qwe123@omniweek9-shard-00-00-y3gvn.mongodb.net:27017,omniweek9-shard-00-01-y3gvn.mongodb.net:27017,omniweek9-shard-00-02-y3gvn.mongodb.net:27017/semana09?ssl=true&replicaSet=omniweek9-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // req.query = acessasr os req params (para filtros)
 // req.params = acessar route params (para edição e deletes)
@@ -13,3 +20,5 @@ app.use(routes);
 
 
 app.listen(3333);
+
+//2019100156992
