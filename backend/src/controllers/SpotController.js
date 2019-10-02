5da51f8fce045342b23b332b1c2915b9cfc2a09e
Fabiano Matheus B Dutra <fmbdutra@gmail.com>
2019-10-01 23:32:35 -0300

    add validacao se usuario existe upload

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,4 +1,5 @@
 const Spot = require('../models/Spot');
+const User = require('../models/User');
 
 module.exports = {
     async store(req, res){
@@ -7,6 +8,14 @@ module.exports = {
        const {user_id} = req.headers;
        //Usuário vai pelo Header e não por qualqer form ou multipart
        //Verá melhor quando se fala em autenticação
+
+       //Validação de usuário
+        const user = await User.findById(user_id);
+
+        if(!user){
+            return res.status(400).json({error: 'Usuario nao encontrado'});
+        } //Erro 400 => bad request
+        //Fim validacao de usuário
 
        const spot = await Spot.create({
            user: user_id,
