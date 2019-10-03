ffb6c4ccdcf0ae41f68e8360d7e4805fa171d0d4
Fabiano Matheus B Dutra <fmbdutra@gmail.com>
2019-10-03 14:33:33 -0300

    fazendo chamadas api ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,9 @@ function App() {
     // document.querySelector('input#email'); assim no JS, mas nao no React
     // console.log(email); // Teste
     const response = await api.post('/sessions', { email });
-    console.log(response);
+
+    const { _id } = response.data;
+    console.log(_id);
 
     
   }
