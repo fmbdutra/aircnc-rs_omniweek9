89eef77046d107d647c61a1211fec9d8e3a9e74a
Fabiano Matheus B Dutra <fmbdutra@gmail.com>
2019-10-04 13:18:28 -0300

    front do /new ok

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import camera from '../../assets/camera.svg';
 
 export default function New(){
-    return <div />
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [prince, setPrice] = useState('');
+    function handleSubmit(){
+
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+          
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa  incrível"
+                value = { company }
+                onChange={event => setCompany(event.target.value)}
+            />
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separados por vírgula)</span> </label>
+            <input
+                id="techs"
+                placeholder="Quais tecnologias usam"
+                value = { techs }
+                onChange={event => setTechs(event.target.value)}
+            />
+            <label htmlFor="prince">VALOR DA DIÁRIA * <span>(caso gratuíto, deixe em branco)</span> </label>
+            <input
+                id="prince"
+                placeholder="Valor cobrado por dia"
+                value = { prince }
+                onChange={event => setPrice(event.target.value)}
+            />
+
+            <button className="btn">Cadastrar</button>
+        </form>
+    )
 }
