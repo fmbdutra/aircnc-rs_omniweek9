e9815be04748e4d13012c76f7c83016ed395eb3b
Fabiano Matheus B Dutra <fmbdutra@gmail.com>
2019-10-04 13:54:49 -0300

    ok botao e preview da imagem

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import camera from '../../assets/camera.svg';
 import './styles.css';
 
 export default function New(){
+    const [thumbnail, setThumbnail] = useState('');
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [prince, setPrice] = useState('');
+    
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null;
+    },[thumbnail])
+    
     function handleSubmit(){
 
     }
+
     return (
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail">
-                <input type="file" />
+            <label 
+                id="thumbnail" 
+                style={{ backgroundImage: `url(${preview})`}}
+                className={thumbnail ? 'has-thumbnail': ''}
+                >
+                <input 
+                    type="file" 
+                    onChange={ event => setThumbnail(event.target.files[0])}/>
                 <img src={camera} alt="Select imagem"  />
             </label>
             <label htmlFor="company">EMPRESA *</label>
